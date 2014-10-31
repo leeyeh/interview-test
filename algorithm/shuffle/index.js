72f251ad862d584186dbdddfbb0541dd042d5726
leeyeh <lee55962698@gmail.com>
2014-10-31 15:04:32 +0800

    add test algorithm/shuffle.

---
+++ algorithm/shuffle/index.js
@@ -0,0 +1,13 @@
+/**
+ * @function shuffle
+ * @description 洗牌算法，将一个数组的元素随机打乱后返回
+ * @param {Array} inputArray
+ * @returns {Array} 打乱后的新数组
+ */
+
+function shuffle(inputArray) {
+	// 请实现 shuffle 方法
+	// 可以 require 任何依赖模块
+}
+
+module.exports = shuffle;
---
+++ algorithm/shuffle/test.js
@@ -0,0 +1,10 @@
+var should = require('should');
+
+var shuffle = require('./index');
+
+describe('shuffle', function() {
+	it('shuffle should be a function.', function() {
+		(typeof shuffle).should.equal('function');
+	});
+	// 下面请测试你的 shuffle 模块
+});
